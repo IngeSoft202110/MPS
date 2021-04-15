b3b9303c26b7a75080d56619f3dfc645b836848d
ActToFool <gordoakm@gmail.com>
2021-04-14 20:32:28 -0500

    modified map

--- lib/views/showRoute.dart
+++ lib/views/showRoute.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:mps/services/google_maps_requests.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:uuid/uuid.dart';
 
 class ShowRoute extends StatefulWidget {
   @override
@@ -26,28 +31,162 @@ class Map extends StatefulWidget {
 
 class _MapState extends State<Map> {
   GoogleMapController mapController;
-  static const _initialPosition = LatLng(6.043144052353413, -75.70493194642403);
+  GoogleMapsServices _googleMapsServices = GoogleMapsServices();
+  TextEditingController locationController = TextEditingController();
+  TextEditingController destination = TextEditingController();
+  static LatLng _initialPosition;
   LatLng _lastPosition = _initialPosition;
   final Set<Marker> _markers = {};
+  final Set<Polyline> _polyLines = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _getUserLocation();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        GoogleMap(
-          initialCameraPosition:
-              CameraPosition(target: _initialPosition, zoom: 17),
-          onMapCreated: onCreated,
-          myLocationEnabled: true,
-          mapType: MapType.normal,
-        )
-      ],
-    );
+    return _initialPosition == null
+        ? Container(
+            alignment: Alignment.center,
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          )
+        : Stack(
+            children: [
+              GoogleMap(
+                initialCameraPosition:
+                    CameraPosition(target: _initialPosition, zoom: 17),
+                onMapCreated: onCreated,
+                myLocationEnabled: true,
+                mapType: MapType.normal,
+                compassEnabled: true,
+                markers: _markers,
+                onCameraMove: _onCameraMove,
+              ),
+              // Positioned(
+              //   top: 40,
+              //   right: 10,
+              //   child: FloatingActionButton(
+              //     onPressed: _onAddMarkerPressed,
+              //     tooltip: "Añadir marcador",
+              //     backgroundColor: Colors.black,
+              //     child: Icon(
+              //       Icons.add_location,
+              //       color: Colors.white,
+              //     ),
+              //   ),
+              // )
+            ],
+          );
   }
 
   void onCreated(GoogleMapController controller) {
     setState(() {
       mapController = controller;
     });
+  }
+
+  void _onCameraMove(CameraPosition position) {
+    setState(() {
+      _lastPosition = position.target;
+    });
+  }
+
+  void _onAddMarkerPressed() {
+    setState(() {
+      _markers.add(Marker(
+          markerId: MarkerId(_lastPosition.toString()),
+          position: _lastPosition,
+          infoWindow: InfoWindow(
+            title: "feo",
+            snippet: "good place",
+          ),
+          icon: BitmapDescriptor.defaultMarker));
+    });
+  }
+
+  //Convert list of doubles into LatLng
+  List<LatLng> converToLatLng(List points) {
+    List<LatLng> result = <LatLng>[];
+    for (int i = 0; i < points.length; i++) {
+      if (i % 2 != 0) {
+        result.add(LatLng(points[i - 1], points[i]));
+      }
+      return result;
+    }
+  }
+
+  List _decodePoly(String poly) {
+    var list = poly.codeUnits;
+    var lList;
+    int index = 0;
+    int len = poly.length;
+    int c = 0;
+    // repeating until all attributes are decoded
+    do {
+      var shift = 0;
+      int result = 0;
+
+      // for decoding value of one attribute
+      do {
+        c = list[index] - 63;
+        result |= (c & 0x1F) << (shift * 5);
+        index++;
+        shift++;
+      } while (c >= 32);
+      /* if value is negetive then bitwise not the value */
+      if (result & 1 == 1) {
+        result = ~result;
+      }
+      var result1 = (result >> 1) * 0.00001;
+      lList.add(result1);
+    } while (index < len);
+
+    /*adding to previous value as done in encoding */
+    for (var i = 2; i < lList.length; i++) lList[i] += lList[i - 2];
+
+    print(lList.toString());
+
+    return lList;
+  }
+
+  void _getUserLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    List<Placemark> placemark =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+    setState(() {
+      _initialPosition = LatLng(position.latitude, position.longitude);
+      locationController.text = placemark[0].name;
+    });
+  }
+
+  void sendRequest(String intendedLocation) async {
+    List<Placemark> placemark;
   }
 }
