e9750159452da010580a2e19680aebe1c59effd5
ActToFool <gordoakm@gmail.com>
2021-04-16 13:52:15 -0500

    overFlow in  SignIn removed

--- lib/views/signin.dart
+++ lib/views/signin.dart
@@ -49,115 +49,119 @@ class _SignInState extends State<SignIn> {
                 child: CircularProgressIndicator(),
               ),
             )
-          : Form(
-              key: _formKey,
-              child: Container(
-                margin: EdgeInsets.symmetric(horizontal: 24),
-                child: Column(
-                  children: [
-                    Container(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 100, vertical: 0),
-                        child: Center(
-                            child:
-                                Image(image: AssetImage('assets/Logo.png')))),
-                    Spacer(),
-                    TextFormField(
-                      validator: (val) {
-                        return val.isEmpty ? "Ingrese Email" : null;
-                      },
-                      decoration: InputDecoration(hintText: "Email"),
-                      onChanged: (val) {
-                        email = val;
-                      },
-                    ),
-                    SizedBox(
-                      height: 5,
-                    ),
-                    TextFormField(
-                      obscureText: true,
-                      validator: (val) {
-                        return val.isEmpty ? "Ingrese la contraseña" : null;
-                      },
-                      decoration: InputDecoration(hintText: "Password"),
-                      onChanged: (val) {
-                        password = val;
-                      },
-                    ),
-                    SizedBox(
-                      height: 14,
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        signIn();
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.indigo[300],
-                          borderRadius: BorderRadius.circular(50),
-                        ),
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                        child: Text(
-                          "Iniciar sesión",
-                          style: TextStyle(
-                            fontSize: 15,
-                            color: Colors.white,
+          : SingleChildScrollView(
+              child: Form(
+                key: _formKey,
+                child: Container(
+                  margin: EdgeInsets.symmetric(horizontal: 24),
+                  child: Column(
+                    children: [
+                      Container(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 100, vertical: 0),
+                          child: Center(
+                              child:
+                                  Image(image: AssetImage('assets/Logo.png')))),
+                      SizedBox(
+                        height: 200,
+                      ),
+                      TextFormField(
+                        validator: (val) {
+                          return val.isEmpty ? "Ingrese Email" : null;
+                        },
+                        decoration: InputDecoration(hintText: "Email"),
+                        onChanged: (val) {
+                          email = val;
+                        },
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        validator: (val) {
+                          return val.isEmpty ? "Ingrese la contraseña" : null;
+                        },
+                        decoration: InputDecoration(hintText: "Password"),
+                        onChanged: (val) {
+                          password = val;
+                        },
+                      ),
+                      SizedBox(
+                        height: 14,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          signIn();
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.indigo[300],
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 10),
+                          child: Text(
+                            "Iniciar sesión",
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 80,
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        AuthMethods().signInWithGoogle(context);
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.indigo[300],
-                          borderRadius: BorderRadius.circular(50),
-                        ),
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                        child: Text(
-                          "Iniciar sesión con Google",
-                          style: TextStyle(
-                            fontSize: 15,
-                            color: Colors.white,
+                      SizedBox(
+                        height: 80,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          AuthMethods().signInWithGoogle(context);
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.indigo[300],
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 10),
+                          child: Text(
+                            "Iniciar sesión con Google",
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 50,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "¿No tienes una cuenta? ",
-                          style: TextStyle(fontSize: 16),
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => SignUp()));
-                          },
-                          child: Text("Regístrate",
-                              style: TextStyle(
-                                  fontSize: 16,
-                                  decoration: TextDecoration.underline)),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                  ],
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "¿No tienes una cuenta? ",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => SignUp()));
+                            },
+                            child: Text("Regístrate",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    decoration: TextDecoration.underline)),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
