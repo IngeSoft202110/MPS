9cd1d99b004fd291fe3f6c36c648bf0affd36c8b
Nicolás Pérez Fonseca <n_perez@javeriana.edu.co>
2021-05-28 01:06:25 -0500

    Revert "Configuracion ios incompleta"
    This reverts commit f371bfb0652237aee4ad6f3a563fb050867f722e.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,6 @@
 import UIKit
 import Flutter
 import GoogleMaps
-import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -11,9 +10,6 @@ import Firebase
   ) -> Bool {
     GMSServices.provideAPIKey("AIzaSyB1bieuxGM_cH12GfAbxJkDVurT-d-z7ZQ")
     GeneratedPluginRegistrant.register(with: self)
-    FirebaseApp.configure()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
-
-
