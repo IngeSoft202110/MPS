bb11a0ee442f3445cb2f60a64bfb3e0d64c7abcf
Bardok1028 <j_calderon@javeriana.edu.co>
2021-06-01 23:14:49 -0500

    chat 2.3

--- lib/views/chatRoomScreen.dart
+++ lib/views/chatRoomScreen.dart
@@ -1,11 +1,9 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:mps/helpfunctions/constant.dart';
 import 'package:mps/helpfunctions/sharedpref_help.dart';
 import 'package:mps/services/auth.dart';
 import 'package:mps/views/conversation_Screen.dart';
-import 'package:mps/views/mainpageuser.dart';
+import 'package:mps/views/homeClient.dart';
 import 'package:mps/views/search_people.dart';
 import 'package:mps/views/widget_chat.dart';
 
@@ -15,71 +13,75 @@ class ChatRoom extends StatefulWidget {
 }
 
 class _ChatRoomState extends State<ChatRoom> {
-
   Stream chatRoomsStream;
 
-  Widget ChatRoomList(){
-      return StreamBuilder(
-        stream: chatRoomsStream,
-        builder: (context, snapshot){
-          return snapshot.hasData ? ListView.builder(
-            itemCount: snapshot.data.documents.length,
-            itemBuilder: (context, index){
-              return ChatRoomsTile(
-                snapshot.data.documents[index].data["chatroomId"]
-                .toString().replaceAll("_", "")
-                .replaceAll(Constant.myName, ""),
-                snapshot.data.documents[index].data["chatroomId"]
-              );
-            }
-          ) : Container();
-        },
-      );
+  Widget ChatRoomList() {
+    return StreamBuilder(
+      stream: chatRoomsStream,
+      builder: (context, snapshot) {
+        return snapshot.hasData
+            ? ListView.builder(
+                itemCount: snapshot.data.documents.length,
+                itemBuilder: (context, index) {
+                  return ChatRoomsTile(
+                      snapshot.data.documents[index].data["chatroomId"]
+                          .toString()
+                          .replaceAll("_", "")
+                          .replaceAll(Constant.myName, ""),
+                      snapshot.data.documents[index].data["chatroomId"]);
+                })
+            : Container();
+      },
+    );
   }
 
   @override
   void initState() {
     getUserInfo();
-    super.initState();  
+    super.initState();
   }
 
-  getUserInfo() async{
+  getUserInfo() async {
     Constant.myName = await SharedPreferenceHelp().getUserName();
-    AuthMethods().getChatRooms(Constant.myName).then((snapshots){
+    AuthMethods().getChatRooms(Constant.myName).then((snapshots) {
       setState(() {
         chatRoomsStream = snapshots;
-        print("we got the data + ${chatRoomsStream.toString()} this is name  ${Constant.myName}");
+        print(
+            "we got the data + ${chatRoomsStream.toString()} this is name  ${Constant.myName}");
       });
     });
-
   }
 
   @override
-  Widget build(BuildContext context) {  
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Image.asset("asset/images/logo_chat.png",
-        height: 50,),
+        title: Image.asset(
+          "asset/images/logo_chat.png",
+          height: 40,
+        ),
+        elevation: 0.0,
+        centerTitle: false,
         actions: [
-          GestureDetector(  
-            onTap: (){
-              Navigator.pushReplacement(context, MaterialPageRoute(
-                builder: (context) => MainPageUser()
-                ));
+          GestureDetector(
+            onTap: () {
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) => HomeClient()));
             },
             child: Container(
                 padding: EdgeInsets.symmetric(horizontal: 16),
-                child: Icon(Icons.exit_to_app)) ,
+                child: Icon(Icons.exit_to_app)),
           ),
         ],
       ),
-      body: ChatRoomList(),
+      body: Container(
+        child: ChatRoomList()
+        ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.search),
-        onPressed: (){
-          Navigator.push(context, MaterialPageRoute(
-            builder: (context) => Search_people()
-            )); 
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => Search_people()));
         },
       ),
     );
@@ -87,40 +89,45 @@ class _ChatRoomState extends State<ChatRoom> {
 }
 
 class ChatRoomsTile extends StatelessWidget {
-
   final String userName;
-  final String chatRoomId; 
+  final String chatRoomId;
   ChatRoomsTile(this.userName, this.chatRoomId);
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector( 
-      onTap: (){
-        Navigator.push(context, MaterialPageRoute(
-          builder: (context) => ConversationScreen(chatRoomId)
-        ));
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => ConversationScreen(chatRoomId)));
       },
-    child: Container(
-      color: Colors.black26,
-      padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-      child: Row(
-        children: [
-          Container(
-            height: 40,
-            width: 40 ,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.circular(40)
+      child: Container(
+        color: Colors.black26,
+        padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+        child: Row(
+          children: [
+            Container(
+              height: 40,
+              width: 40,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  color: Colors.blue, borderRadius: BorderRadius.circular(40)),
+              child: Text(
+                "${userName.substring(0, 1).toUpperCase()}",
+                style: mediumTextStyle(),
+              ),
             ),
-            child: Text("${userName.substring(0,1).toUpperCase()}",
-            style: mediumTextStyle(),),
-          ),
-          SizedBox(width: 8,),
-          Text(userName, style: mediumTextStyle(),)
-        ],
+            SizedBox(
+              width: 8,
+            ),
+            Text(
+              userName,
+              style: mediumTextStyle(),
+            )
+          ],
+        ),
       ),
-    ),
-  );
-}
+    );
+  }
 }
